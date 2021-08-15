3e7a55df4a81ab225683c0b835a4eb8dcfb6769d
SirLynx_ <jackdaw141@gmail.com>
2021-08-15 14:48:31 +0100

    Adding models, lang file, textures.

--- src/main/java/io/github/sirlynx/vanillaextended/VanillaExtended.java
+++ src/main/java/io/github/sirlynx/vanillaextended/VanillaExtended.java
@@ -1,15 +1,15 @@
 package io.github.sirlynx.vanillaextended;
 
+import io.github.sirlynx.vanillaextended.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 public class VanillaExtended implements ModInitializer {
     public static final String MOD_ID = "vanillaextended";
 
     public VanillaExtended() {
-
     }
 
     public void onInitialize() {
-
+        ModItems.registerItems();
     }
 }
